8dfd4dd712e2f4fc8defe60973962c4601d936f1
Sergio <sergio.scar56@gmail.com>
2022-07-06 20:25:09 -0300

    Ajuste

--- arrays/colecoes/Hash.java
+++ arrays/colecoes/Hash.java
@@ -11,6 +11,8 @@ public class Hash {
         users.add(new Usuario("nome2"));
 
         Boolean resultado = users.contains(new Usuario("nome2"));
-        System.out.println(resultado);// Se você comentar o hashcode da classes usuário, o resultado retornara falso, 
+        System.out.println(resultado);
+        // Se você comentar o hashcode da classes usuário, o resultado retornara falso, 
         //porque como o contains vai utilizar o equals e o equals usa o HASHCODE, com isso ira retornar falso.
+    }
 }
